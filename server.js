30638cf46a59f3593488e0adfd852a5ff8b03a01
masonjar <mason.whitaker79@gmail.com>
2022-04-08 08:37:15 -0700

    fixed syntax error

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors());
-const mongoose = require(mongoose);
+const mongoose = require("mongoose");
 const db = mongoose.connection;
 const Book = require("./models/books");
 
