a4014a284bb6cfff4a1a5f83e7bf333c6e3a6648
masonjar <mason.whitaker79@gmail.com>
2022-04-08 08:27:14 -0700

    Create /books route to check what is in the DB

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(cors());
 const mongoose = require(mongoose);
-const db = mongoose.connection
+const db = mongoose.connection;
+const Book = require("./models/books");
 
 mongoose.connect(process.env.MONGO_CONNECT);
-db.on('error', console.error.bind(console, "connection error: "))
-db.once('open', _ => {
-  console.log('Mongo Atlas connection sucessful');
-})
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", (_) => {
+  console.log("Mongo Atlas connection sucessful");
+});
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
-
-  response.send('test request received')
-
-})
+app.get("/test", (request, response) => {
+  response.send("test request received");
+});
+app.get("/books", getBooks);
+async function getBooks(request, response) {
+  try {
+    const books = await Book.find({});
+    response.status(200).send(books);
+  } catch (e) {
+    console.error(e);
+    response.status(500).send("1nterna1 5ervar 3rrar")
+  }
+}
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
